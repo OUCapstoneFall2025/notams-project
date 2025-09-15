ddf02b11c3ef95ad4a7627dfe068d2227f9172a1
Blake Wood <blakewood28@yahoo.com>
2025-09-14 20:13:59 -0500

    feat: Java 21 toolchain + app module CLI skeleton (args echo) + JUnit 5

---
+++ app/src/test/notams/AppTest.java
@@ -0,0 +1,12 @@
+package edu.ou.capstone.notams;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+    @Test
+    void sanity() {
+        assertEquals(2, 1 + 1);
+    }
+}
+
